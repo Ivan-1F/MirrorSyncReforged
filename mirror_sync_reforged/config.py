18ef936cc463b35fe580c32c65450fcc21367c88
Ivan1F <wyf0762@foxmail.com>
2022-03-13 00:17:14 +0800

    added type annotations to config.py to make things work

--- mirror_sync_reforged/config.py
+++ mirror_sync_reforged/config.py
@@ -1,9 +1,11 @@
+from typing import List
+
 from mcdreforged.api.all import *
 
 
 class Configure(Serializable):
-    permission_level = PermissionLevel.PHYSICAL_SERVER_CONTROL_LEVEL
-    survival_server_path = '../survival/server'
-    mirror_server_path = './server'
-    world_names = ['world']
-    count_down = 10
+    permission_level: int = PermissionLevel.PHYSICAL_SERVER_CONTROL_LEVEL
+    survival_server_path: str = '../survival/server'
+    mirror_server_path: str = './server'
+    world_names: List[str] = ['world']
+    count_down: int = 10
